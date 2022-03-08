8f4b9a979e5baabd1b3dee98f439dea196fd9de2
Pedro Henrique Linhares <phlinharesaraujo@gmail.com>
2022-03-08 17:08:51 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,18 @@
+import{ReactComponent as GithubIcon} from 'assets/img/github.svg'
+import './styles.css'
+function Navbar(){
+    return(   <header>
+        <nav className="container">
+          <div className='dsmovie-nav-content'>
+            <h1 >DSmovie</h1>
+            <a href="https://github.com/Pedrohla14">
+            <div className='dsmovie-contact-container'>
+              <GithubIcon/>
+              <p className="dsmovie-contact-link">/Pedrohla14</p>
+            </div>
+            </a>
+          </div>
+        </nav>
+      </header>);
+}
+export default Navbar;
